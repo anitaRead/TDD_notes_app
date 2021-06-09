7dc349034922b119750e4916034e521f775d1bba
anitaRead <areadman91@gmail.com>
2021-06-09 09:29:57 +0100

    Display list of titles

--- lib/notes.rb
+++ lib/notes.rb
@@ -1,8 +1,16 @@
 class Note
 
+    attr_reader :list_of_titles
+
+    def initialize
+        @list_of_titles = []
+    end
+
     def title
         puts "Enter the title of your note: "
-        gets.chomp
+        user_input = gets.chomp
+        @list_of_titles << user_input
+
     end
 
     def body
--- spec/notes_spec.rb
+++ spec/notes_spec.rb
@@ -4,10 +4,18 @@ describe Note do
     # User can add a note with a title and a body
 
     it 'can add a title' do
-        expect(subject.title).to eq("a note")
+        expect(subject.title).to eq("Law abiding citizen")
     end
     it 'can add a body' do
         expect(subject.body).to eq("body")
+    end
+
+    # User can see a list of all note titles
+
+    it 'can display list of titles' do
+        subject.title
+        subject.title
+        expect(subject.list_of_titles).to eq ["Children of men", "Law abiding citizen"]
     end
 
 end
